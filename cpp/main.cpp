63f70aa2dabfe4b62f10b0c6f5aaa12414d263b2
Hulej Attila <attila.hulej@tttsolutions.hu>
2023-12-30 17:06:30 +0100

    added comment to processing variables

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -10,11 +10,11 @@ int main(int argc, char **argv) {
 
     std::string filePath = argv[1];
 
-    int minRadius = 50;
-    int maxRadius = 100;
-    int cannyThreshold = 100;
-    int accumulatorThreshold = 50;
-    int maxFrameMissingTracking = 10;
+    int minRadius = 50; // Minimum radius of detected circles. Set this based on the smallest circle size you expect to detect.
+    int maxRadius = 100; // Maximum radius of detected circles. Set this based on the largest circle size you expect to detect.
+    int cannyThreshold = 100; // The higher threshold of the two passed to the Canny edge detector. A lower value will detect more edges, while a higher value might miss some.
+    int accumulatorThreshold = 50; // Accumulator threshold for the circle centers at the detection stage. The smaller it is, the more false circles may be detected.
+    int maxFrameMissingTracking = 20; // Maximum number of frames a circle can be missing before the track is considered lost. Increase this for more lenient tracking, decrease for stricter tracking.
 
     // Try to open as an image first
     cv::Mat image = cv::imread(filePath, cv::IMREAD_COLOR);
