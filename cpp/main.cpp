74e2a3300de10538a071ec5557dded0348f42630
Hulej Attila <attila.hulej@tttsolutions.hu>
2023-12-30 15:19:32 +0100

    improved detection

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,71 +1,113 @@
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgproc.hpp>
-#include <vector>
+#include <iostream>
+
+/**
+ * Processes an image or a frame from a video to detect circles using the Hough Circle Transform.
+ * This function first converts the input image to grayscale, then applies a Gaussian blur to reduce noise,
+ * and finally uses the Hough Circle Transform to find circles in the image. Detected circles are drawn
+ * on the original image.
+ *
+ * @param src The source image on which circle detection and drawing will be performed. It is modified in place.
+ * @param minRadius The minimum radius of circles to be detected. If unknown, set to a reasonable lower bound or 0.
+ * @param maxRadius The maximum radius of circles to be detected. If unknown, set to a reasonable upper bound or 0.
+ * @param minDist The minimum distance between the centers of the detected circles. This parameter helps to ensure
+ *                that the algorithm doesn't detect multiple adjacent circles as one.
+ * @param param1 The higher threshold of the two passed to the Canny edge detector (the lower one is twice smaller).
+ *               It is used by the Hough Circle Transform internally.
+ * @param param2 The accumulator threshold for the circle centers at the detection stage. The smaller it is, the more
+ *               false circles may be detected. Circles corresponding to the larger accumulator values will be returned first.
+ */
+void processFrame(cv::Mat &src, int minRadius, int maxRadius, float minDist, int param1, int param2){
+    cv::Mat gray, blur, edges;
+    // Convert to grayscale
+    cvtColor(src, gray, cv::COLOR_BGR2GRAY);
+
+    // Apply Gaussian blur to smooth the image
+    GaussianBlur(gray, blur, cv::Size(9, 9), 2, 2);
+
+    // Use the Hough transform to detect circles in the blurred image
+    std::vector<cv::Vec3f> circles;
+    HoughCircles(blur, circles, cv::HOUGH_GRADIENT, 1, minDist, param1, param2, minRadius, maxRadius);
+
+    // Draw the circles detected
+    for (const auto & i : circles) {
+        cv::Vec3i c = i;
+        cv::Point center = cv::Point(c[0], c[1]);
+        // Circle center
+        circle(src, center, 1, cv::Scalar(0, 100, 100), 3, cv::LINE_AA);
+        // Circle outline
+        int radius = c[2];
+        circle(src, center, radius, cv::Scalar(255, 0, 255), 3, cv::LINE_AA);
+    }
+}
 
 int main(int argc, char** argv) {
-    // Load image
     if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " <ImagePath>\n";
+        std::cout << "Usage: " << argv[0] << " <path_to_media>" << std::endl;
         return -1;
     }
 
-    // Load image from the provided path
-    cv::Mat image = cv::imread(argv[1]);
-    if (image.empty()) {
-        std::cout << "Could not open or find the image!\n";
-        return -1;
-    }
+    std::string filePath = argv[1];
+    cv::Mat src;
+    int minRadius = 50;
+    int maxRadius = 100;
+    float minDist = src.rows / 8; // This will be set properly once we know the image dimensions
+    int param1 = 100;
+    int param2 = 50;
 
-    // Convert to grayscale
-    cv::Mat gray;
-    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+    // Check the file extension to determine if it's an image or a video
+    std::string fileExtension = filePath.substr(filePath.find_last_of(".") + 1);
 
-    // Apply Gaussian Blur
-    cv::GaussianBlur(gray, gray, cv::Size(9, 9), 2, 2);
+    if (fileExtension == "png" || fileExtension == "jpg" || fileExtension == "jpeg") {
+        // It's an image
+        src = cv::imread(filePath, cv::IMREAD_COLOR);
 
-    // Dynamic thresholding for edge detection
-    // Compute the median of the single channel pixel intensities
-    double median;
-    cv::Mat sorted;
-    cv::sort(gray.reshape(0, 1), sorted, cv::SORT_EVERY_ROW + cv::SORT_ASCENDING);
-    median = sorted.at<uchar>(sorted.total() / 2);
-    double sigma = 0.33;
-    int lower = int(std::max(0.0, (1.0 - sigma) * median));
-    int upper = int(std::min(255.0, (1.0 + sigma) * median));
-
-    cv::Mat edges;
-    cv::Canny(gray, edges, lower, upper);
-
-    // Find contours
-    std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(edges, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-
-    // Process contours
-    for (const auto & contour : contours) {
-        double area = cv::contourArea(contour);
-        double perimeter = cv::arcLength(contour, true);
-        double circularity = 4 * CV_PI * area / (perimeter * perimeter);
-        std::cout << "Circularity: " << circularity << std::endl;
-        std::cout << "Area: " << area << std::endl;
-
-        // Check if contour is circular (circularity close to 1) and area is bigger then 100
-        if (circularity > 0.7 && circularity < 1.2 && area > 100.0) {
-            // Find the minimum enclosing circle
-            cv::Point2f center;
-            float radius;
-            cv::minEnclosingCircle(contour, center, radius);
-
-            // Draw the circle
-            cv::circle(image, center, static_cast<int>(radius), cv::Scalar(0, 255, 0), 2);
-
-            // Draw the center point
-            cv::circle(image, center, 2, cv::Scalar(0, 0, 255), -1);
+        // Check if image is loaded fine
+        if (src.empty()) {
+            std::cout << "Error opening image!" << std::endl;
+            return -1;
         }
-    }
 
-    // Display or save the result
-    cv::imshow("Detected Target", image);
-    cv::waitKey(0);
+        // Set the minimum distance between detected centers based on the image size
+        minDist = src.rows / 8;
+
+        processFrame(src, minRadius, maxRadius, minDist, param1, param2);
+
+        // Show the final result with detected circles
+        cv::imshow("Detected Circles", src);
+        cv::waitKey(0); // Wait for any key press to close
+
+    } else if (fileExtension == "mp4" || fileExtension == "avi") {
+        // It's a video
+        cv::VideoCapture cap(filePath);
+
+        // Check if video is loaded fine
+        if (!cap.isOpened()) {
+            std::cout << "Error opening video stream or file!" << std::endl;
+            return -1;
+        }
+
+        // Read the first frame to set the minimum distance between detected centers
+        cap.read(src);
+        if (src.empty()) {
+            std::cout << "Error reading video stream or file!" << std::endl;
+            return -1;
+        }
+        minDist = src.rows / 8;
+
+        // Process video stream
+        do {
+            processFrame(src, minRadius, maxRadius, minDist, param1, param2);
+
+            // Show the result with detected circles
+            cv::imshow("Detected Circles", src);
+            if (cv::waitKey(30) >= 0) break; // Wait for 30ms or key press to move to next frame
+
+        } while (cap.read(src));
+    } else {
+        std::cout << "File format not recognized." << std::endl;
+        return -1;
+    }
 
     return 0;
 }
